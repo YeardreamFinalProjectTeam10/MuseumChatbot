cb240146df579cebc3f86c891fc847f370c55981
Hyojeong <itshyoj@gmail.com>
2024-01-18 19:53:52 +0900

    Refactor: Modularize preprocessing code
    1. 로우데이터에서 시대, 국적, 보관번호 추출 후 Description으로 합치는 코드
    2. LangChain의 TextSplitter를 활용하여 토큰 갯수 미만으로 Split하는 코드
    를 한 번에 모듈화하여 Main 함수로 실행할 수 있게끔 함

---
+++ gendata/save_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def save_jsonl(data, filepath):
+    df = pd.DataFrame(data)
+    df.to_json(f"{filepath}.jsonl", orient='records', lines=True, force_ascii=False)
+
+def save_log_csv(data, filepath):
+    df = pd.DataFrame(data)
+    df.to_csv(f'{filepath}.csv', encoding='utf-8-sig')
