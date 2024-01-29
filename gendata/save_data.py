84376aaff87afebe64aa827cc566629a9bde9346
Hyojeong <itshyoj@gmail.com>
2024-01-29 18:54:37 +0900

    Feat: Create a module to store CSV files and JSONL files

--- gendata/save_data.py
+++ gendata/save_data.py
@@ -4,6 +4,6 @@ def save_jsonl(data, filepath):
     df = pd.DataFrame(data)
     df.to_json(f"{filepath}.jsonl", orient='records', lines=True, force_ascii=False)
 
-def save_log_csv(data, filepath):
+def save_csv(data, filepath):
     df = pd.DataFrame(data)
     df.to_csv(f'{filepath}.csv', encoding='utf-8-sig')
