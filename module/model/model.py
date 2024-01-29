9cae9d2425c21b2ac87a43fbd996ea4e25db5eda
Hyojeong <itshyoj@gmail.com>
2024-01-30 00:13:38 +0900

    Remove and Rename: Delete and move unneeded code

--- module/model/model.py
+++ module/model/model.py
@@ -1,14 +1,18 @@
-from transformers import BertModel, BertTokenizerFast, Trainer, TrainingArguments
+from transformers import AutoTokenizer, AutoModel, BertModel, BertTokenizerFast, Trainer, TrainingArguments
+from kobert_tokenizer import KoBERTTokenizer
 import torch
 from torch import nn
 
 
-class kdpr(nn.Module):
-    def __init__(self):
+class Model(nn.Module):
+    def __init__(self, tokenizer):
         super().__init__()
-        self.tokenizer = BertTokenizerFast.from_pretrained('klue/bert-base')
-        self.question_encoder = BertModel.from_pretrained('klue/bert-base')
-        self.passage_encoder = BertModel.from_pretrained('klue/bert-base')
+
+        self.tokenizer = tokenizer
+        model_name = tokenizer.name_or_path
+
+        self.question_encoder = BertModel.from_pretrained(model_name)
+        self.passage_encoder = BertModel.from_pretrained(model_name)
 
         self.passage_dropout = nn.Dropout(0.1)
         self.question_dropout = nn.Dropout(0.1)
@@ -17,8 +21,6 @@ class kdpr(nn.Module):
 
     def forward(self, q_input_ids, q_attention_mask, q_token_type_ids,
                 p_input_ids, p_attention_mask, p_token_type_ids):
-
-
 
         question_output = self.question_encoder(input_ids=q_input_ids, attention_mask=q_attention_mask, token_type_ids=q_token_type_ids)
         passage_output = self.passage_encoder(input_ids=p_input_ids, attention_mask=p_attention_mask, token_type_ids=p_token_type_ids)
@@ -29,6 +31,4 @@ class kdpr(nn.Module):
         question_embeddings = torch.stack([question_output[i][0] for i in range(len(question_output))])
         passage_embeddings = torch.stack([passage_output[i][0] for i in range(len(passage_output))])
 
-
-        #return self.m(torch.matmul(question_embeddings,passage_embeddings.T))
         return question_embeddings, passage_embeddings
